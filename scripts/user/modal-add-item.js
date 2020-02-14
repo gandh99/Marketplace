71591a98d0029d7d1ddefcca342bbf7d6deaa063
gandh99 <gandh99@hotmail.com>
2020-02-14 22:38:48 +0800

    Added image preview for selected file

--- scripts/user/modal-add-item.js
+++ scripts/user/modal-add-item.js
@@ -1,33 +1,25 @@
 import { addItemUrl } from '../routes.js';
 
+// Submit the item to be added on the server
 let addItemButton = document.getElementById('add-item-button');
 addItemButton.addEventListener('click', () => {
     let image = document.getElementById('file-upload').files[0];
     let itemName = document.getElementById('item-name');
     let itemPrice = document.getElementById('item-price');
-    console.log(image);
-    upload(image);
 });
 
-function upload(file) {
-    const reader = new FileReader();
-    const xhr = new XMLHttpRequest();
-    
-    const self = this;
-    xhr.upload.addEventListener("progress", function (e) {
-        if (e.lengthComputable) {
-            const percentage = Math.round((e.loaded * 100) / e.total);
-        }
-    }, false);
+// Allow the image to be previewed
+let fileUpload = document.getElementById('modal-file-upload');
+fileUpload.onchange = () => {
+    console.log(fileUpload)
+    if (fileUpload.files && fileUpload.files[0]) {
+        var reader = new FileReader();
 
-    xhr.upload.addEventListener("load", function (e) {
+        reader.onload = (e) => {
+            $('#modal-image-preview')
+                .attr('src', e.target.result);
+        };
 
-    }, false);
-    xhr.open("POST", addItemUrl);
-    xhr.onload = () => { console.log(xhr.response) };
-    xhr.overrideMimeType('text/plain; charset=x-user-defined-binary');
-    reader.onload = function (evt) {
-        xhr.send(evt.target.result);
-    };
-    reader.readAsBinaryString(file);
+        reader.readAsDataURL(fileUpload.files[0]);
+    }
 }
