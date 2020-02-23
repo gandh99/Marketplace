387605fd6d3f8f5366bef45404144992e8d00dd5
gandh99 <gandh99@hotmail.com>
2020-02-23 12:43:24 +0800

    Refactored my-items.js by creating MyItemsHolder in my-items-holder.js. This class will be responsible for creating and displaying the items.

---
+++ scripts/user/my-items-holder.js
@@ -0,0 +1,91 @@
+import ConfirmationModal from '../components/confirmation-modal.js';
+import { deleteItem } from './my-items.js';
+
+export default class MyItemsHolder {
+    constructor(itemsArea, messageArea, itemArray) {
+        this.itemArray = itemArray;
+        this.messageArea = messageArea;
+        this.itemsArea = itemsArea;
+    }
+
+    displayItems(itemArray) {
+        this.removeAllItems();
+        this.displayMessage('');
+
+        if (itemArray.length == 0) {
+            this.displayMessage('No items to show.');
+        }
+
+        for (let item of itemArray) {
+            this.createItem(item);
+        }
+    }
+
+    createItem(item) {
+        // Create the html elements
+        let itemCard = document.createElement('div');
+        let itemCardImage = document.createElement('div');
+        let itemCardTitle = document.createElement('div');
+        let itemCardPrice = document.createElement('div');
+        let deleteButton = document.createElement('div');
+
+        // Assign the necessary attributes
+        itemCard.setAttribute('class', 'item-card');
+        itemCardImage.setAttribute('class', 'item-card-image');
+        itemCardTitle.setAttribute('class', 'item-card-title');
+        itemCardPrice.setAttribute('class', 'item-card-price');
+        deleteButton.setAttribute('class', 'item-card-delete-button');
+
+        // Add the item data
+        this.createItemImage(itemCardImage, item.item_image_base64);
+        itemCardTitle.innerHTML = item.item_name;
+        itemCardPrice.innerHTML = 'S$' + item.item_price;
+        deleteButton.innerHTML = 'Remove Item';
+        deleteButton.addEventListener('click', () => {
+            this.displayConfirmationModal(deleteButton, item);
+        });
+
+        // Attach to item card
+        itemCard.appendChild(itemCardImage);
+        itemCard.appendChild(itemCardTitle);
+        itemCard.appendChild(itemCardPrice);
+        itemCard.appendChild(deleteButton);
+
+        // Put item card in items area
+        this.itemsArea.appendChild(itemCard);
+    }
+
+    // Create an image from the base64Image and append it to the itemCardImage
+    createItemImage(itemCardImage, base64Image) {
+        let image = new Image();
+        image.src = base64Image;
+        image.style.width = '80%';
+        itemCardImage.appendChild(image);
+    }
+
+    filterBySearchValue(searchValue) {
+        let filteredItems = [];
+        for (let item of this.itemArray) {
+            if (item.item_name.includes(searchValue)) {
+                filteredItems.push(item);
+            }
+        }
+
+        this.displayItems(filteredItems);
+    }
+
+    removeAllItems() {
+        this.itemsArea.innerHTML = '';
+    }
+
+    displayMessage(message) {
+        this.messageArea.innerHTML = message;
+    }
+
+    displayConfirmationModal(button, item) {
+        const title = 'Delete Item';
+        const message = 'Are you sure you wish to delete this item?';
+        const deleteFunction = function () { deleteItem(item) };
+        new ConfirmationModal().show(button, title, message, deleteFunction);
+    }
+}
--- scripts/user/my-items.js
+++ scripts/user/my-items.js
@@ -1,10 +1,12 @@
 import { getItemUrl, deleteItemUrl } from '../routes.js';
 import { getToken } from '../authentication/jwt.js';
-import ConfirmationModal from '../components/confirmation-modal.js';
+import MyItemsHolder from './my-items-holder.js';
 
 const itemsArea = document.getElementsByClassName('items-area')[0];
-let itemArray;
+const messageArea = document.getElementsByClassName('message-area')[0];
+let myItemsHolder;
 
+clearInputs();
 retrieveMyItems();
 
 // Get the user's list of items from the server
@@ -14,8 +16,9 @@ function retrieveMyItems() {
     xhr.setRequestHeader('Authorization', 'Bearer ' + getToken());
     xhr.onload = () => {
         if (xhr.status == 200) {
-            itemArray = JSON.parse(xhr.response);
-            displayItems(itemArray);
+            let itemArray = JSON.parse(xhr.response);
+            myItemsHolder = new MyItemsHolder(itemsArea, messageArea, itemArray);
+            myItemsHolder.displayItems(itemArray);
         } else if (xhr.status == 403) {
             let message = 'Please login to view your items';
             displayMessage(message);
@@ -25,87 +28,8 @@ function retrieveMyItems() {
     xhr.send();
 }
 
-// Create an image from the base64Image and append it to the itemCardImage
-function createItemImage(itemCardImage, base64Image) {
-    let image = new Image();
-    image.src = base64Image;
-    image.style.width = '80%';
-    itemCardImage.appendChild(image);
-}
-
-// Items is an array of JSON objects
-function displayItems(itemArray) {
-    removeAllItems();
-    displayMessage('');
-
-    if (itemArray.length == 0) {
-        displayMessage('No items to show.');
-    }
-
-    for (let item of itemArray) {
-        createItem(item);
-    }
-}
-
-// Removes all items displayed
-function removeAllItems() {
-    itemsArea.innerHTML = '';
-}
-
-// Create the item to be attached to the itemsArea
-function createItem(item) {
-    // Create the html elements
-    let itemCard = document.createElement('div');
-    let itemCardImage = document.createElement('div');
-    let itemCardTitle = document.createElement('div');
-    let itemCardPrice = document.createElement('div');
-    let deleteButton = document.createElement('div');
-
-    // Assign the necessary attributes
-    itemCard.setAttribute('class', 'item-card');
-    itemCardImage.setAttribute('class', 'item-card-image');
-    itemCardTitle.setAttribute('class', 'item-card-title');
-    itemCardPrice.setAttribute('class', 'item-card-price');
-    deleteButton.setAttribute('class', 'item-card-delete-button');
-
-    // Add the item data
-    createItemImage(itemCardImage, item.item_image_base64);
-    itemCardTitle.innerHTML = item.item_name;
-    itemCardPrice.innerHTML = 'S$' + item.item_price;
-    deleteButton.innerHTML = 'Remove Item';
-    deleteButton.addEventListener('click', () => {
-        displayConfirmationModal(deleteButton, item);
-    });
-
-    // Attach to item card
-    itemCard.appendChild(itemCardImage);
-    itemCard.appendChild(itemCardTitle);
-    itemCard.appendChild(itemCardPrice);
-    itemCard.appendChild(deleteButton);
-
-    // Put item card in items area
-    itemsArea.appendChild(itemCard);
-}
-
-function displayMessage(message) {
-    const messageArea = document.getElementsByClassName('message-area')[0];
-    messageArea.innerHTML = message;
-}
-
-function hideUtilityBar() {
-    const utilityBar = document.getElementsByClassName('utility-bar')[0];
-    utilityBar.style.display = 'none';
-}
-
-function displayConfirmationModal(button, item) {
-    const title = 'Delete Item';
-    const message = 'Are you sure you wish to delete this item?';
-    const deleteFunction = function () { deleteItem(item) };
-    new ConfirmationModal().show(button, title, message, deleteFunction);
-}
-
 // Delete the item by supplying its item_id to the server
-function deleteItem(item) {
+export function deleteItem(item) {
     const xhr = new XMLHttpRequest();
     xhr.open('DELETE', deleteItemUrl + '/' + item.item_id);
     xhr.setRequestHeader('Authorization', 'Bearer ' + getToken());
@@ -120,29 +44,22 @@ function deleteItem(item) {
     xhr.send();
 }
 
+function hideUtilityBar() {
+    const utilityBar = document.getElementsByClassName('utility-bar')[0];
+    utilityBar.style.display = 'none';
+}
+
 // Implement search functionality 
 let searchTerm = document.getElementsByClassName('searchTerm')[0];
 searchTerm.addEventListener('keyup', () => {
     let searchValue = searchTerm.value;
-    let filteredItems = filterBySearchValue(searchValue);
-    displayItems(filteredItems);
+    myItemsHolder.filterBySearchValue(searchValue);
 });
-
-function filterBySearchValue(searchValue) {
-    let filteredItems = [];
-    for (let item of itemArray) {
-        if (item.item_name.includes(searchValue)) {
-            filteredItems.push(item);
-        }
-    }
-
-    return filteredItems;
-}
 
 // Infinite scroll
 var scrollContainer = document.getElementsByClassName('items-area')[0];
 var infiniteScroll = new InfiniteScroll(scrollContainer, {
-    // options
+    // Options
     path: '.pagination__next',
     append: '.post',
     history: false,
