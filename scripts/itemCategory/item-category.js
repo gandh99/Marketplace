f11d5867f9e7757affbc60536b33c85aa607ea9e
gandh99 <gandh99@hotmail.com>
2020-02-24 21:12:18 +0800

    Created client script to fetch item information from the server and create HTML elements using the information. However, the images have yet to be fetched.

--- scripts/itemCategory/item-category.js
+++ scripts/itemCategory/item-category.js
@@ -2,22 +2,66 @@ import { itemCategoryUrl } from '../routes.js';
 import { getToken } from '../authentication/jwt.js';
 
 export function loadItems(category) {
-    let itemArray = getItemsFromServer(category);
-    const itemsArea = document.getElementsByClassName('items-area')[0];
+    getItemsFromServer(category)
+        .then(itemArray => {
+            displayItems(itemArray);
+        })
 }
 
 function getItemsFromServer(category) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', itemCategoryUrl + category);
-    xhr.onload = () => {
-        if (xhr.status == 200) {
-            let itemArray = xhr.response;
-            console.log(itemArray);
-        } else {
-            displayMessage('Oops! Something went wrong. Please try again later.');
-        }
-    };
-    xhr.send();
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', itemCategoryUrl + category);
+        xhr.onload = () => {
+            if (xhr.status == 200) {
+                let itemArray = xhr.response;
+                resolve(JSON.parse(itemArray));
+            } else {
+                displayMessage('Oops! Something went wrong. Please try again later.');
+            }
+        };
+        xhr.send();
+    })
+}
+
+function displayItems(itemArray) {
+    for (let item of itemArray) {
+        createItem(item);
+    }
+}
+
+function createItem(item) {
+    const itemsArea = document.getElementsByClassName('items-area')[0];
+
+    // Create div
+    let itemContainer = document.createElement('div');
+    let itemCardImage = document.createElement('div');
+    let itemName = document.createElement('div');
+    let itemPrice = document.createElement('div');
+
+    // Assign class
+    itemContainer.setAttribute('class', 'item-container');
+    itemCardImage.setAttribute('class', 'item-image');
+    itemName.setAttribute('class', 'item-name');
+    itemPrice.setAttribute('class', 'item-price');
+
+    // Add the data
+    // createItemImage(itemCardImage, item.xx);
+    itemName.innerHTML = item.item_name;
+    itemPrice.innerHTML = item.item_price;
+
+    // Append to itemsArea
+    itemContainer.appendChild(itemCardImage);
+    itemContainer.appendChild(itemName);
+    itemContainer.appendChild(itemPrice);
+    itemsArea.appendChild(itemContainer);
+}
+
+function createItemImage(itemCardImage, base64Image) {
+    let image = new Image();
+    image.src = base64Image;
+    image.style.width = '80%';
+    itemCardImage.appendChild(image);
 }
 
 function displayMessage(message) {
